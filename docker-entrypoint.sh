7c51c8af52992a544712c7ce734a8e144d5285ba
Alex Potts <lex.potsus@gmail.com>
2017-10-25 19:44:20 -0400

    wait a second for postgres to start

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,5 @@ pip install -r requirements.txt
 python manage.py migrate
 #might want to modify this to clear the database, or have this run on build
 python manage.py loaddata initial_data.json
+sleep 3 #TODO: this is just a quick fix to make it easier to pick up the postgres connection
 exec "$@"
